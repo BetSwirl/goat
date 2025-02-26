6285645aa029bf837e8b547f61a3e2ef1d41eccf
Romuald <romuald@betswirl.com>
2025-02-26 11:31:05 +0100

    feat(examples): add BetSwirl

---
+++ typescript/examples/vercel-ai/betswirl/CHANGELOG.md
@@ -0,0 +1,1 @@
+# goat-examples-vercel-ai-betswirl
---
+++ typescript/examples/vercel-ai/betswirl/index.ts
@@ -0,0 +1,73 @@
+import readline from "node:readline";
+
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
+
+import { http } from "viem";
+import { createWalletClient } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { base } from "viem/chains";
+
+import { getOnChainTools } from "@goat-sdk/adapter-vercel-ai";
+
+import { betswirl } from "../../../packages/plugins/betswirl"
+
+import { viem } from "@goat-sdk/wallet-viem";
+
+require("dotenv").config();
+
+const account = privateKeyToAccount(process.env.WALLET_PRIVATE_KEY as `0x${string}`);
+
+const walletClient = createWalletClient({
+    account: account,
+    transport: http(process.env.RPC_PROVIDER_URL),
+    chain: base,
+});
+
+(async () => {
+    const tools = await getOnChainTools({
+        wallet: viem(walletClient),
+        plugins: [
+            betswirl()
+        ],
+    });
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    while (true) {
+        const prompt = await new Promise<string>((resolve) => {
+            rl.question('Enter your prompt (or "exit" to quit): ', resolve);
+        });
+
+        if (prompt === "exit") {
+            rl.close();
+            break;
+        }
+
+        console.log("\n-------------------\n");
+        console.log("TOOLS CALLED");
+        console.log("\n-------------------\n");
+
+        console.log("\n-------------------\n");
+        console.log("RESPONSE");
+        console.log("\n-------------------\n");
+        try {
+            const result = await generateText({
+                model: openai("gpt-4o-mini"),
+                tools: tools,
+                maxSteps: 10, // Maximum number of tool invocations per request
+                prompt: prompt,
+                onStepFinish: (event) => {
+                    console.log(event.toolResults);
+                },
+            });
+            console.log(result.text);
+        } catch (error) {
+            console.error(error);
+        }
+        console.log("\n-------------------\n");
+    }
+})();
